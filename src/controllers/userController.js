8ed1a08a93e633918a65cf310bbd0c6f61edf064
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-04-06 23:09:38 +0530

    'USER REGISTRATION API, Iss commit me USER REGISTRATION API ko CLASS Oriented Code se FUNCTIONAL Oriented code me convert kiya gaya hai

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,13 @@
-import UserRepository from "../repository/userRepository.js";
-import UserService from "../service/userService.js";
+import { registerUserService } from "../service/userService.js";
 
 async function createUserController(req, res) {
   console.log("Create user controller called...");
   console.log(req.body);
 
   // TOD0: Register the user --> Done ho gaya ab
-  const userService = new UserService(new UserRepository());
 
   try {
-    const response = await userService.registerUserService(req.body);
+    const response = await registerUserService(req.body);
 
     return res.status(200).json({
       message: "Successfully registered the user",
--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,24 +1,22 @@
 import User from "../schema/userSchema.js";
 
-class UserRepository {
-  async findUserRepo(parameters) {
-    try {
-      const response = await User.findOne({ ...parameters });
+async function findUserRepo(parameters) {
+  try {
+    const response = await User.findOne({ ...parameters });
 
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async createUserRepo(userDetails) {
-    try {
-      const response = await User.create(userDetails);
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
+    return response;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-export default UserRepository;
+async function createUserRepo(userDetails) {
+  try {
+    const response = await User.create(userDetails);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { findUserRepo, createUserRepo };
